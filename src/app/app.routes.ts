69d7f2b60c2c3a1abef49175ce838eecf8517371
Mario Garcia <mariogarrod@gmail.com>
2026-05-21 15:31:11 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    loadComponent: () =>
-      import('./pages/login/login.component').then(c => c.LoginComponent)
+    path: '',
+      component: LoginComponent,
   }
 ];
