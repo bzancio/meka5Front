72f0fb182a6b3db425d9406af44d6c05f5e7a877
Mario Garcia <mariogarrod@gmail.com>
2026-05-22 11:00:24 +0200

    login login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  constructor(private http: HttpClient) {}
+
+  login() {
+    const user = (document.getElementById('user') as HTMLInputElement).value;
+    const password = (document.getElementById('pass') as HTMLInputElement).value;
+
+    this.http.post('/login', { user, password }).subscribe();
+  }
+
+  newUser() {
+    const user = (document.getElementById('user') as HTMLInputElement).value;
+    const password = (document.getElementById('pass') as HTMLInputElement).value;
+
+    this.http.post('/newUser', { user, password }).subscribe();
+  }
+}
