93f88e7bba2b0c81604e2934aabb5f8cbbe38de2
Mario Garcia <mariogarrod@gmail.com>
2026-05-22 11:10:46 +0200

    Revert "login login"
    This reverts commit 72f0fb182a6b3db425d9406af44d6c05f5e7a877.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
-  constructor(private http: HttpClient) {}
-
-  login() {
-    const user = (document.getElementById('user') as HTMLInputElement).value;
-    const password = (document.getElementById('pass') as HTMLInputElement).value;
-
-    this.http.post('/login', { user, password }).subscribe();
-  }
-
-  newUser() {
-    const user = (document.getElementById('user') as HTMLInputElement).value;
-    const password = (document.getElementById('pass') as HTMLInputElement).value;
-
-    this.http.post('/newUser', { user, password }).subscribe();
-  }
-}
+export class LoginComponent {}
