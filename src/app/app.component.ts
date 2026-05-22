1af7af04a3081c3f820fe2a5c806ee5619639af0
Mario Garcia <mariogarrod@gmail.com>
2026-05-22 12:45:34 +0200

    login logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
+export class App{
   protected readonly title = signal('Meka5');
 }
