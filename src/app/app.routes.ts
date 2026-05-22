830ff59ae557a38890cffeb4036c7ec8b0daca15
Mario Garcia <mariogarrod@gmail.com>
2026-05-22 13:01:47 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
- 
+
 import { LoginComponent } from './pages/login/login.component';
 
 import { HomeComponent } from './pages/home/home.component';
@@ -9,9 +9,9 @@ export const routes: Routes = [
     path: '',
       component: LoginComponent,
   },
-  
+
   {
-  path: '', 
+  path: '',
     component: HomeComponent,
   },
 
