bc32f75ff7e30c6466a61fa58047fdce8cf90a80
plushtrap00 <pablete.merejo@gmail.com>
2026-05-26 10:41:07 +0200

    merge arreglado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/Header/header.component";
+import { FooterComponent } from "../../shared/Footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
