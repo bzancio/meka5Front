ae4f503904363c6405eeeece5062f65d3a0353b4
plushtrap00 <pablete.merejo@gmail.com>
2026-05-26 12:30:21 +0200

    test apis

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 import { HeaderComponent } from "../../shared/Header/header.component";
 import { FooterComponent } from "../../shared/Footer/footer.component";
 
@@ -9,8 +10,17 @@ import { FooterComponent } from "../../shared/Footer/footer.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private http = inject(HttpClient);
+  private partialUrl = 'http://localhost:8080';
 
-  testText: string =
-    'El texto no está asique toma emojis 🤣🤣🤣🤣👌🤣👌🤣😭😭😭😭🕔👌👌👌👌';
+  text = signal<string>('');
+
+  ngOnInit(): void {
+    this.http.get(this.partialUrl + '/Hello?name=Mario Folleti', { responseType: 'text' })
+    .subscribe({
+      next: (response) => this.text.set(response),
+      error: (err) => console.error('Error', err)
+    });
+  }
 }
