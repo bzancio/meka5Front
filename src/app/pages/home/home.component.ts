c16e3687cb7d0ca1b3daabdaaa5256f25fa8394a
plushtrap00 <pablete.merejo@gmail.com>
2026-05-29 14:42:59 +0200

    instalación de bootstrap, api words conectada Y typerFuncional con muestro de datos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,13 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/Header/header.component";
 import { FooterComponent } from "../../shared/Footer/footer.component";
+import { CommonWordsComponent } from "../../components/common-words/common-words.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonWordsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  private http = inject(HttpClient);
-  private partialUrl = 'http://localhost:8080';
-
-  text = signal<string>('');
-
-  ngOnInit(): void {
-    this.http.get(this.partialUrl + '/Hello?name=Mario Folleti', { responseType: 'text' })
-    .subscribe({
-      next: (response) => this.text.set(response),
-      error: (err) => console.error('Error', err)
-    });
-  }
-}
+export class HomeComponent {}
